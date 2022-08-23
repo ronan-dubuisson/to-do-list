d6465c6791c6878e5db6d7a77262693a7255bbc5
Ronan Dubuisson <ronandubuisson@Ronans-MacBook-Pro.local>
2022-08-23 10:49:34 +0200

    configures eslint and prettier

--- app.js
+++ app.js
@@ -1,66 +1,69 @@
-//declaring modules
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const date = require(__dirname + "/date.js");
-const database = require(__dirname + "/db.js");
-//TODO: Breng default values naar local file
-const dbConnectionString = "mongodb://localhost:27017/toDoListDB";
+// declaring modules
+require('ejs');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// eslint-disable-next-line import/no-dynamic-require
+const date = require(`${__dirname}/date.js`);
+// eslint-disable-next-line import/no-dynamic-require
+const database = require(`${__dirname}/db.js`);
+// TODO: Breng default values naar local file
+const dbConnectionString = 'mongodb://localhost:27017/toDoListDB';
+// eslint-disable-next-line new-cap
 const db = new database(dbConnectionString);
-const defaultTodoItems = ["Welcome To Your To Do List", "Hit the + button to add a new item", " <-- check to mark the item as done", "hit the - button to delete an item"];
+const defaultTodoItems = ['Welcome To Your To Do List', 'Hit the + button to add a new item', ' <-- check to mark the item as done', 'hit the - button to delete an item'];
 
-//setting up express and its uses
+// setting up express and its uses
 const app = express();
-app.set("view engine", "ejs"); //view engine for templating
-app.use(bodyParser.urlencoded({extended: true})); //enabeling parsing of body for requests
-app.use(express.static("public")); //defining folder for static files to be served by express
+app.set('view engine', 'ejs'); // view engine for templating
+app.use(bodyParser.urlencoded({ extended: true })); // enabeling parsing of body for requests
+app.use(express.static('public')); // defining folder for static files to be served by express
 
-app.get("/", (req, res) => {
-    //using the then as async functions return promises
-    db.getAllItems().then((items) => {
-
-        if(items.length===0){
-            //if no items are in the db, add default items
-            db.addItem(defaultTodoItems).then(()=>{
-                    res.redirect("/");
-            });
-        } else {
-            //reading out the items and render it to the page
-            res.render("list", {
-                listTitle: date.getDate(),
-                listItems: items
-            });
-        }
-    })
+app.get('/', (req, res) => {
+  // using the then as async functions return promises
+  db.getAllItems().then((items) => {
+    if (items.length === 0) {
+      // if no items are in the db, add default items
+      db.addItem(defaultTodoItems).then(() => {
+        res.redirect('/');
+      });
+    } else {
+      // reading out the items and render it to the page
+      res.render('list', {
+        listTitle: date.getDate(),
+        listItems: items,
+      });
+    }
+  });
 });
 
-app.post("/", (req, res) => {
-    db.addItem([req.body.newItem]).then(() => {
-        //only do the redirect after the promise is fullfilled.
-        res.redirect("/");
+app.post('/', (req, res) => {
+  db.addItem([req.body.newItem]).then(() => {
+    // only do the redirect after the promise is fullfilled.
+    res.redirect('/');
+  });
+});
+
+app.get('/:customListName', (req, res) => {
+  const { customListName } = req.params;
+  db.getAllItems().then((items) => {
+    res.render('list', {
+      listTitle: customListName,
+      listItems: items,
     });
+  });
 });
 
-app.get("/:customListName",(req, res)=>{
-    const customListName = req.params.customListName;
-    db.getAllItems().then((items)=>{
-        res.render("list",{
-            listTitle: customListName,
-            listItems: items
-        })
-    })
-})
-
-app.post("/delete",(req, res)=>{
-    db.deleteItemById(req.body.checkbox).then(()=>{
-        res.redirect("/");
-    });
-})
-
-app.get("/about", (req, res) => {
-    res.render("about");
+app.post('/delete', (req, res) => {
+  db.deleteItemById(req.body.checkbox).then(() => {
+    res.redirect('/');
+  });
 });
 
-app.listen("3000", () => {
-    console.log("Server running");
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.listen('3000', () => {
+  console.log('Server running');
 });
--- db.js
+++ db.js
@@ -1,73 +1,74 @@
-//module declaration
-const mongoose = require("mongoose");
+// module declaration
+const mongoose = require('mongoose');
 
 let conn;
 
-//DB schemas
-//TODO: breng db schemas naar local files?
-const itemSchema = new mongoose.Schema ({
-    name: {
-        type: String,
-        required: true
-    },
+// DB schemas
+// TODO: breng db schemas naar local files?
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
 });
 
-const listSchema = new mongoose.Schema ({
-    name: {
-        type: String,
-        required: true
-    },
-    items: [itemSchema]
+const listSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  items: [itemSchema],
 });
 
-//DB Models
-const Item = mongoose.model("item", itemSchema);
-const List = mongoose.model("list", listSchema);
+// DB Models
+const Item = mongoose.model('item', itemSchema);
+const List = mongoose.model('list', listSchema);
 
-module.exports = Db;
-function Db(connectionString){
-    this.connectionString = connectionString;
+function Db(connectionString) {
+  this.connectionString = connectionString;
 }
 
 async function openConnection(connectionString) {
-   conn = await mongoose.connect(connectionString);
-}
-
-Db.prototype.getAllItems = getAllItems;
-async function getAllItems() {
-    await openConnection(this.connectionString);
-    //finding all documents
-    const items = await Item.find({}).exec();
-
-    await closeConnection();
-    return items;
-}
-
-Db.prototype.addItem = addItem;
-async function addItem(itemNames) {
-    //opening db connection
-    await openConnection(this.connectionString);
-
-    //Creating the items documents
-    const items = [];
-    itemNames.forEach((itemName)=>{
-        items.push({
-            name:itemName
-        });
-    });
-
-    //add items to the db
-    await Item.insertMany(items);
-    await closeConnection();
-}
-
-Db.prototype.deleteItemById = deleteItemById;
-async function deleteItemById(id){
-    await openConnection(this.connectionString);
-    await Item.findByIdAndDelete(id);
-    await closeConnection();
+  conn = await mongoose.connect(connectionString);
 }
 
 async function closeConnection() {
-    conn.disconnect();
+  conn.disconnect();
 }
+
+async function getAllItems() {
+  await openConnection(this.connectionString);
+  // finding all documents
+  const items = await Item.find({}).exec();
+
+  await closeConnection();
+  return items;
+}
+
+async function addItem(itemNames) {
+  // opening db connection
+  await openConnection(this.connectionString);
+
+  // Creating the items documents
+  const items = [];
+  itemNames.forEach((itemName) => {
+    items.push({
+      name: itemName,
+    });
+  });
+
+  // add items to the db
+  await Item.insertMany(items);
+  await closeConnection();
+}
+
+async function deleteItemById(id) {
+  await openConnection(this.connectionString);
+  await Item.findByIdAndDelete(id);
+  await closeConnection();
+}
+
+module.exports = Db;
+Db.prototype.addItem = addItem;
+Db.prototype.getAllItems = getAllItems;
+Db.prototype.deleteItemById = deleteItemById;
